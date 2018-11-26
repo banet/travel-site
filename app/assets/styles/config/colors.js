b3a7f62493e98731fe839c9c7942fe9802174d17
Bane <bane.tepavcevic@gmail.com>
2018-11-26 13:57:44 +0100

    large-hero is styled

---
+++ app/assets/styles/config/colors.js
@@ -0,0 +1,3 @@
+module.exports = {
+    Mainblue: '#056ef0'
+}
--- gulpfile.js
+++ gulpfile.js
@@ -1,45 +1,41 @@
 // Here you can write variables without repeating var ... 
-
 var gulp = require('gulp'),
-watch = require('gulp-watch'),
-postcss = require('gulp-postcss'),
-autorpefixer = require('autoprefixer'),
-// you can named variables what ever you want
-cssvars = require('postcss-simple-vars'),
-nestedcss = require('postcss-nested');
-
+    watch = require('gulp-watch'),
+    postcss = require('gulp-postcss'),
+    autoprefixer = require('autoprefixer'),
+    // you can named variables what ever you want
+    cssvars = require('postcss-simple-vars'),
+    nested = require('postcss-nested'),
+    // colors = require('./app/assets/styles/config/colors'),
+    cssImport = require('postcss-import');
 
 //Gulp task-test
 //1 argument task name , 2 argument task runs
-gulp.task('default', function() {
-    console.log("Hooray you created Gulp-task");
-})
-// HTML task
-gulp.task('html', function(){
-    console.log('Imagine something useful being to your HTML here');
+gulp.task('default', function () {
+    console.log("Hooray - you created a Gulp task.");
+});
 
-})
+// HTML task
+gulp.task('html', function () {
+    console.log("Imagine something useful being done to your HTML here.");
+});
 // Style task
 gulp.task('styles', function () {
-    
-   return gulp.src('./app/assets/styles/styles.css')
-       .pipe(postcss([nestedcss, cssvars, autorpefixer]))
-    .pipe(gulp.dest('./app/temp/styles'));
-    
-
+    return gulp.src('./app/assets/styles/styles.css')
+        .pipe(postcss([cssImport, cssvars,  nested, autoprefixer]))
+        .pipe(gulp.dest('./app/temp/styles'));
 });
+
 
 // Gulp Watch
 gulp.task('watch', function () {
     //1 arg to watch file we want to save
     watch('./app/index.html', function () {
         gulp.start('html');
-    })
+    });
 
-    //1 arg to watch file we want to save
     watch('./app/assets/styles/**/*.css', function () {
         gulp.start('styles');
-    })
+    });
 
 });
-
